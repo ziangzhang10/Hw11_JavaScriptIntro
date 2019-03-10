9cd8732619f17e27c5e3feb2b7a53abcece6387c
ZiAng <ziang.rice@gmail.com>
2019-03-09 22:18:53 -0600

    added optional (dropdown menu)

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -18,6 +18,24 @@ tableData.forEach((sighting) => {
 // Get a reference to the filter button
 var button = d3.select("#filter-btn");
 
+  
+// Shapes to select from (dropdown menu)
+var data = ["", "triangle", "rectangle", "cigar", "circle", "disk", "sphere", "cylinder", "chevron", "teardrop", "light", "fireball", "formation", "other", "unknown"];
+
+// get content from dropdown menu
+var select = d3.select('#shape')
+    .append('select')
+        .attr('class','select')
+      .on('change',onchange);
+
+// This creates the dropdown menu
+var options = select
+      .selectAll('option')
+        .data(data).enter()
+        .append('option')
+            .text(function (d) { return d; });
+
+
 
 button.on("click", function() {
 
@@ -30,19 +48,30 @@ button.on("click", function() {
     var inputElement = d3.select("#datetime");
   
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    var datetimeValue = inputElement.property("value");
+    //console.log(datetimeValue);
+
+    // Select the input element and get the input date
+    var inputElement = d3.select("#city");
   
-    console.log(inputValue);
+    // Get the value property of the input element
+    var cityValue = inputElement.property("value");
 
     // clear original table
     d3.selectAll("tbody tr").remove()
 
+    // shape of the UFO from the dropdown menu
+	shapeValue = d3.select('select').property('value')
+    console.log(shapeValue);
+
     // loop through array to find matching data
     tableData.forEach((sighting) => {
         var row = tbody.append("tr");
-        console.log(sighting.datetime);
-        // if date match
-        if (sighting.datetime === inputValue)
+
+        // if date & city & shape match; empty is OK
+        if ((sighting.datetime === datetimeValue || datetimeValue == "")
+            && (sighting.city === cityValue || cityValue == "")
+            && (sighting.shape === shapeValue || shapeValue == ""))
         {
             Object.entries(sighting).forEach(([key, value]) => {
                 var cell = row.append("td");
@@ -52,4 +81,8 @@ button.on("click", function() {
     });
 
 });
-  
+
+
+function onchange() {
+	selectValue = d3.select('select').property('value')
+};
